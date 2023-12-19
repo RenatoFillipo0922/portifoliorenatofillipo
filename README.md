f05d542c0a84c7ac8f8cacf4cf34be0601cbccda
Renato <renatofillipo09@gmail.com>
2023-12-19 12:09:54 -0300

    initial-commit

---
+++ README.md
@@ -0,0 +1,40 @@
+<h1 align="center"> Porfitólio Renato Fillipo </h1>
+
+<p align="center">
+Este foi um projeto onde estão minhas informações e também os projetos que foram desenvolvidos.<br/>
+</p>
+
+<p align="center">
+  <a href="#-tecnologias">Tecnologias</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-projeto">Projeto</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#memo-licença">Licença</a>
+</p>
+
+<p align="center">
+  <img alt="License" src="https://img.shields.io/static/v1?label=license&message=MIT&color=49AA26&labelColor=000000">
+</p>
+
+<br>
+
+<p align="center">
+  <img alt="projeto Habits" src=".github/preview.png" width="100%">
+</p>
+
+## 🚀 Tecnologias
+
+Esse projeto foi desenvolvido com as seguintes tecnologias:
+
+- HTML e CSS
+- JavaScript
+- Git e Github
+
+
+## 💻 Projeto
+
+Este é um portifólio onde contem informações e projetos.
+
+- [Visite o projeto online](https://renatofillipo0922.github.io/portifoliorenatofillipo)
+
+## :memo: Licença
+
+Esse projeto está sob a licença MIT.
---
+++ main.js
@@ -0,0 +1,112 @@
+/* abre e fecha o menu quando clicar no icone: hamburguer e x */
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+
+/* quando clicar em um item do menu, esconder o menu */
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
+
+/* mudar o header da página quando der scroll */
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+function changeHeaderWhenScroll() {
+  if (this.window.scrollY >= navHeight) {
+    // scroll é maior que a altura do header
+    header.classList.add('scroll')
+  } else {
+    // menor que a altura do header
+    header.classList.remove('scroll')
+  }
+}
+
+
+/* Menu Ativo conforme a secção visível na página */
+const sections = document.querySelectorAll('main section[id]')
+function activateMenuAtCurrentSection() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+  for (const section of sections) {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.add('active')
+    } else {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.remove('active')
+    }
+  }
+}
+
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `
+  #home .texto, #home .titulo,
+  #about .image, #about .text,
+  #experience header, #experience p,
+  #projects header, #projects .cards,
+  #contact .container .grid, #contact .links
+  footer .brand, footer .social
+  `,
+  { interval: 50 }
+)
+
+const backToTopButton = document.querySelector('.back-to-top')
+
+function backToTop() {
+  if (this.window.scrollY >= 560) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
+
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+}
+
+
+function typeWriter(elemento) {
+  const textoArray = elemento.innerHTML.split('')
+  elemento.innerHTML = '';
+  textoArray.forEach((letra, i) => {
+    setTimeout(() => elemento.innerHTML += letra, 75 * i)
+  });
+}
+
+const subtitulo = document.getElementById('subtitulo')
+typeWriter(subtitulo)
+
+/*When Scroll */
+window.addEventListener('scroll', function () {
+  changeHeaderWhenScroll()
+  backToTop()
+  activateMenuAtCurrentSection()
+})
